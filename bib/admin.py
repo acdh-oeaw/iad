93fc7e6f53d144c1a91b42fabf988dbd5b4be2c9
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-03-15 10:48:57 +0100

    added script to import zotero data dump

--- bib/admin.py
+++ bib/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    search_fields = [
+        'zoterokey', 'item_type', 'author', 'title', 'publication_title',
+        'publication_year', 'isbn', 'url'
+    ]
+    list_display = [
+        'zoterokey', 'item_type', 'author', 'title', 'publication_title',
+        'publication_year', 'isbn', 'url'
+    ]
+
+
+admin.site.register(Book, BookAdmin)
 
 # Register your models here.
--- bib/models.py
+++ bib/models.py
@@ -4,25 +4,25 @@ from django.conf import settings
 
 
 class Book(models.Model):
-    zoterokey = models.CharField(max_length=100, primary_key=True)
-    item_type = models.CharField(max_length=100, blank=True, null=True)
-    author = models.CharField(max_length=100, blank=True, null=True)
-    title = models.CharField(max_length=500, blank=True, null=True)
-    publication_title = models.CharField(max_length=100, blank=True, null=True)
+    zoterokey = models.CharField(max_length=500, primary_key=True)
+    item_type = models.CharField(max_length=500, blank=True, null=True)
+    author = models.CharField(max_length=500, blank=True, null=True)
+    title = models.TextField(blank=True, null=True)
+    publication_title = models.CharField(max_length=500, blank=True, null=True)
     short_title = models.CharField(max_length=500, blank=True, null=True)
     publication_year = models.IntegerField(blank=True, null=True)
-    place = models.CharField(max_length=100, blank=True, null=True)
-    isbn = models.CharField(max_length=100, blank=True, null=True)
-    issn = models.CharField(max_length=100, blank=True, null=True)
-    doi = models.CharField(max_length=100, blank=True, null=True)
-    url = models.CharField(max_length=100, blank=True, null=True)
+    place = models.CharField(max_length=500, blank=True, null=True)
+    isbn = models.CharField(max_length=500, blank=True, null=True)
+    issn = models.CharField(max_length=500, blank=True, null=True)
+    doi = models.CharField(max_length=500, blank=True, null=True)
+    url = models.CharField(max_length=500, blank=True, null=True)
 
     def get_zotero_url(self):
         "Returns the objects URL pointing to its Zotero entry"
         try:
             base = "https://www.zotero.org/{}/".format(settings.Z_ID_TYPE)
-            url = "{}{}/peter_handke_stage_texts/items/itemKey/{}".format(
-                base, settings.Z_ID, self.zoterokey
+            url = "{}/items/itemKey/{}".format(
+                settings.Z_COLLECTION_URL, self.zoterokey
             )
             return url
         except AttributeError:
