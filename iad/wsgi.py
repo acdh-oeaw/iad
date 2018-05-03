b08da7aeb2b091b1e95b0e00fd09e92316d6c538
Peter Andorfer <peter.andorfer@oeaw.ac.at>
2018-05-03 10:22:42 +0200

    order matters!!!

--- iad/wsgi.py
+++ iad/wsgi.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
-from django.core.wsgi import get_wsgi_application
-
 sys.path.append('/var/www/html')
 sys.path.append('/var/www/html/myenv/lib/python3.6/site-packages')
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "iad.settings.server"
 
