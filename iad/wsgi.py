ddc92694610cdb92240436ab01e45d4a00d7bbdf
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-05-03 10:11:38 +0200

    changed wsgi

--- iad/wsgi.py
+++ iad/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for iad project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iad.settings")
+sys.path.append('/var/www/html')
+sys.path.append('/var/www/html/myenv/lib/python3.6/site-packages')
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "iad.settings.server"
 
 application = get_wsgi_application()
