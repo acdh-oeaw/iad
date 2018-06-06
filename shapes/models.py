edc9237d71990c4d50bb8960b4eba497c09d5df8
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-06-06 11:03:52 +0200

    removed nuts2 from shapes.models.CadastralCommunity str-method

--- shapes/models.py
+++ shapes/models.py
@@ -52,10 +52,13 @@ class CadastralCommunity(models.Model):
     geom = models.MultiPolygonField(blank=True, null=True)
 
     def __str__(self):
-        if self.nuts3_name and self.nuts2_name and self.cadcom_nam and self.state_name:
-            return "{} >> {} >> {} ({})".format(
-                self.cadcom_nam, self.nuts3_name, self.nuts2_name, self.state_name
+        if self.nuts3_name and self.cadcom_nam and self.state_name:
+            return "{} >> {} ({})".format(
+                self.cadcom_nam, self.nuts3_name, self.state_name
             )
+
+    def __str__(self):
+        return "{}".format(self.id)
 
     def get_absolute_url(self):
         return reverse(
@@ -96,15 +99,15 @@ cadastralcommunity_mapping = {
     'cadcom_num': 'CadCom_num',
     'cadcom_nam': 'CadCom_nam',
     'cadcom_alt': 'CadCom_alt',
-    'lau2_muni_field': 'LAU2_Muni_',
-    'lau2_mun_1': 'LAU2_Mun_1',
-    'polbez_num': 'PolBez_num',
-    'polbez_nam': 'PolBez_nam',
+    # 'lau2_muni_field': 'LAU2_Muni_',
+    # 'lau2_mun_1': 'LAU2_Mun_1',
+    # 'polbez_num': 'PolBez_num',
+    # 'polbez_nam': 'PolBez_nam',
     'nuts3_code': 'NUTS3_code',
     'nuts3_name': 'NUTS3_name',
-    'bundesland': 'Bundesland',
-    'nuts2_name': 'NUTS2_name',
-    'nuts2_code': 'NUTS2_code',
+    # 'bundesland': 'Bundesland',
+    # 'nuts2_name': 'NUTS2_name',
+    # 'nuts2_code': 'NUTS2_code',
     'state_code': 'STATE_code',
     'state_name': 'STATE_name',
     'state_altn': 'STATE_altn',
