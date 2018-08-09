d59047e148003d6dd6734bbb605012f317969f5d
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-08-09 09:55:41 +0200

    style settings added #5

---
+++ archiv/styles.py
@@ -0,0 +1,27 @@
+SITE_STYLE = {
+    "color": "#ff7800",
+    "weight": 2,
+    "opacity": 1,
+    "fillOpacity": 0,
+}
+
+ARCHENT_STYLE = {
+    "color": "#????",
+    "weight": 2,
+    "opacity": 1,
+    "fillOpacity": 0,
+}
+
+RESEARCHEVENT_STYLE = {
+    "color": "#????",
+    "weight": 2,
+    "opacity": 1,
+    "fillOpacity": 0,
+}
+
+MONUMENTPROTECTION_STYLE = {
+    "color": "#????",
+    "weight": 2,
+    "opacity": 1,
+    "fillOpacity": 0,
+}
