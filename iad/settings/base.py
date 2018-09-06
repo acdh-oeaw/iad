cabdb8e8b9636cb4043792991dd4246dec4d16fe
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-09-06 13:57:31 +0200

    minor

--- iad/settings/base.py
+++ iad/settings/base.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'webpage.webpage_content_processors.installed_apps',
+                'webpage.webpage_content_processors.is_dev_version',
             ],
         },
     },
