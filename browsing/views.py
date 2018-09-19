0698fe082f15dea2139e155c37bbf2e26b490513
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-09-19 10:23:35 +0200

    added map to archiv-browsing views

--- browsing/views.py
+++ browsing/views.py
@@ -1,6 +1,8 @@
 import time
 import datetime
 from django.http import HttpResponse
+from django.core.serializers import serialize
+from django.contrib.contenttypes.models import ContentType
 import rdflib
 from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef, RDFS, ConjunctiveGraph
 from django_tables2 import SingleTableView, RequestConfig
@@ -29,7 +31,7 @@ class GenericListView(SingleTableView):
     filter_class = None
     formhelper_class = None
     context_filter_name = 'filter'
-    paginate_by = 25
+    paginate_by = 10
     template_name = 'browsing/generic_list.html'
 
     def get_queryset(self, **kwargs):
@@ -46,6 +48,35 @@ class GenericListView(SingleTableView):
 
     def get_context_data(self, **kwargs):
         context = super(GenericListView, self).get_context_data()
+        ct = ContentType.objects.get(
+            model=self.model.__name__.lower()
+        ).model_class()
+        try:
+            ct._meta.get_field('polygon')
+            poly = True
+        except Exception as e:
+            poly = False
+        if poly:
+            points = serialize(
+                'geojson',
+                self.get_queryset(),
+                geometry_field='centroid',
+                fields=(
+                    'name',
+                    'pk',
+                )
+            )
+            context['points'] = points
+            shapes = serialize(
+                'geojson',
+                self.get_queryset(),
+                geometry_field='polygon',
+                fields=(
+                    'name',
+                    'pk',
+                )
+            )
+            context['shapes'] = shapes
         context[self.context_filter_name] = self.filter
         context['docstring'] = "{}".format(self.model.__doc__)
         if self.model._meta.verbose_name_plural:
