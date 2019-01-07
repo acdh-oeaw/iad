90c5f79cc840348df01fb6600cd1c7c4b38d1103
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-01-07 12:09:30 +0100

    added ksenia to authors

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -3,7 +3,7 @@
 
 PROJECT_METADATA = {
     'title': 'IRON-AGE-DANUBE',
-    'author': 'Peter Andorfer',
+    'author': 'Peter Andorfer, Ksenia Zaytseva',
     'subtitle': 'MONUMENTALIZED EARLY IRON AGE LANDSCAPES IN THE DANUBE RIVER BASIN',
     'description': 'Over the past few decades, European archaeological heritage gained major attention by connecting prominent archaeological sites to wider transnational networks, which have become a popular tool for their protection, promotion and tourist use. Imposingly attractive, however very fragile, prehistoric landscapes in the Danube programme area remain partly hidden and not well integrated into cultural tourism. The Iron-Age-Danube project focuses on monumental archaeological landscapes of the Early Iron Age, characterized by, e.g., fortified hilltop settlements and large tumulus cemeteries, from the era between roughly the 9th–4th cent. BC (Hallstatt period). At that time, the Danube region was part of a cultural phenomenon called the Eastern-Hallstatt circle.',
     'github': 'https://github.com/acdh-oeaw/iad',
