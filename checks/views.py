97ec6f89ac5e2a807d92f7fa2e87adfb9844e531
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-02-01 09:51:37 +0100

    minor things in quality checks

--- checks/views.py
+++ checks/views.py
@@ -52,7 +52,7 @@ class PolygonExists(TemplateView):
         model_name = self.kwargs['model_name']
         entity_model = ContentType.objects.get(app_label='archiv', model=model_name).model_class()
         no_poly = entity_model.objects.filter(polygon=None)
-        context['no_poly'] = no_poly[:50]
+        context['no_poly'] = no_poly
         context['no_poly_count'] = no_poly.count()
         context['all'] = entity_model.objects.all().count()
         context['class'] = entity_model._meta.verbose_name
