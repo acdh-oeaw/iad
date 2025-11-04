9fb20e83b0bbdef36d190f49fdfcedc392614ef0
csae8092 <p.andorfer@gmail.com>
2025-11-04 09:50:46 +0100

    fix(docker): startserver should use uv

--- start-server.sh
+++ start-server.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 # start-server.sh
 echo "Hello from IAD"
-python manage.py collectstatic --no-input
+uv run manage.py collectstatic --no-input
 if [ -n "$MIGRATE" ] ; then
     (echo "making migrations and running them"
-    python manage.py makemigrations --no-input
-    python manage.py migrate --no-input)
+    uv run manage.py makemigrations --no-input
+    uv run manage.py migrate --no-input)
 fi
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (echo "creating superuser ${DJANGO_SUPERUSER_USERNAME}" && python manage.py createsuperuser --no-input --noinput --email 'blank@email.com')
+    (echo "creating superuser ${DJANGO_SUPERUSER_USERNAME}" && uv run manage.py createsuperuser --no-input --noinput --email 'blank@email.com')
 fi
-echo "Check cache table"
-python manage.py createcachetable
-gunicorn iad.wsgi --user www-data --bind 0.0.0.0:8010 --timeout 360 --workers 3 & nginx -g "daemon off;"
+uv run gunicorn iad.wsgi --user www-data --bind 0.0.0.0:8010 --timeout 360 --workers 3 & nginx -g "daemon off;"
